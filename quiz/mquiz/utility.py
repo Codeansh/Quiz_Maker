143e0a2101fb1a7cd33662bb57dc812599626e32
Codeansh <ansh02shiv@gmail.com>
2022-09-14 01:32:44 +0530

    Replacing Pymongo With MongoClient

--- quiz/mquiz/utility.py
+++
@@ -1,9 +0,0 @@
-def add_solvers(title, solvers):  # list of solvers
-    qzs = quizes.find_one({'_id': title})
-    l = qzs.get('solvers')
-    if l:
-        l.extend(solvers)
-        l = list(set(l))
-    else:
-        l = list(set(solvers))
-    quizes.update_one({'_id': title}, {'$set': {'solvers': l}})
